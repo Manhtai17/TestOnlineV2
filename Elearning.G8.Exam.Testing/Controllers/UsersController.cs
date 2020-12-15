20aed47faec359671376ba4fcdf39c0855396716
manh pham <manhtai17@gmail.com>
2020-12-15 23:00:41 +0700

    api user

---
+++ Elearning.G8.Exam.Testing/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Elearning.G8.Exam.ApplicationCore;
+using Elearning.G8.Exam.Testing.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Elearning.G8.Exam.Testing.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : BaseController<User>
+	{
+		private readonly IBaseEntityService<User> _baseEntityService;
+
+		public UsersController(IBaseEntityService<User> baseEntityService):base(baseEntityService)
+		{
+
+		}
+	}
+}
